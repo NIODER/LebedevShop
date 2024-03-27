2b73f45d4713f95b68c325297c7b582d475b5acb
NIODER <nioder125@gmail.com>
2024-03-27 18:16:05 +0300

    add api.py

--- statistics/api.py
+++ statistics/api.py
@@ -1,8 +1,43 @@
+import requests
+
+API_URL = "http://localhost:8080"
+
+
 def get_items():
-    # get items. .... http .. gert
-    return []
+    return requests.get(API_URL + '/items', ).json()
 
-def delete_item(id):
-    # delete /items/id
-    return
+def create_order():
+    return requests.put(url=API_URL + '/order', json=
+    {
+        'address': 'some address',
+        'basketId': ''
+    }).json()
 
+def create_item():
+    return requests.put(url=API_URL + '/items', json=
+    {
+        'itemName': 'item1',
+        'description': 'cool item!',
+        'cost': 123.123
+    }).json()
+
+def delete_item(item_id):
+    return requests.delete(API_URL + f'/items/{item_id}').json()
+
+def get_item_by_id(item_id):
+    return requests.get(API_URL + f'/items/{item_id}').json()
+
+def basket_add_item(basket_id, item_id):
+    return requests.post(API_URL + f'/basket/{basket_id}/{item_id}').json()
+
+def create_basket():
+    return requests.put(API_URL + '/basket').json()
+
+def delete_basket_by_id(basket_id):
+    return requests.delete(API_URL + f'/basket/{basket_id}').json()
+
+def delete_item_from_basket(basket_id, item_id):
+    return requests.delete(API_URL + f'/basket/{basket_id}/{item_id}').json()
+
+def get_basket_by_id(basket_id):
+    return requests.get(API_URL + f'/basket/{basket_id}').json()
--- statistics/scenarios.py
+++
@@ -1,6 +0,0 @@
-from api import get_items, delete_item
-
-
-def get_delete():
-    get_items()
-    delete_item(id)
